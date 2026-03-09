0cb1fb4d924d80d244f07d25055ba5fdc41acca7
Chumar <chumarsarwar3@gmail.com>
2026-03-09 16:07:15 +0500

    responsive bar

--- src/components/Annocementbar.jsx
+++ src/components/Annocementbar.jsx
@@ -5,8 +5,8 @@ const Annocementbar = () => {
     <div className="mt-16 md:mt-20 2xl:mt-25 3xl:mt-28 bg-[#828282] py-3.75 xl:py-8 2xl:py-10 lg:py-6 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-45   ">
       <p
         className="
-      text-[clamp(15px,1.45vw,39px)]
-        leading-[100%] tracking-[0%] font-normal font-family-aeonik xl:ml-28 lg:ml-28 "
+      text-[clamp(14.75px,1.45vw,39px)]
+        leading-[100%] tracking-[0%] font-normal font-family-aeonik xl:ml-28 lg:ml-26 "
       >
         Use AI to identify and record the food you weigh and log calories
         automatically. World's First Launch!
