cdad20dd6bcf7af492ce5999234189a78c7fdd8f
Chumar <chumarsarwar3@gmail.com>
2026-03-17 19:51:59 +0500

    herosection make perfect

--- src/components/MeetQalzy.jsx
+++ src/components/MeetQalzy.jsx
@@ -1,6 +1,6 @@
 const MeetQalzy = () => {
   return (
-    <section className="relative w-full bg-white  overflow-hidden lg:mt-[clamp(-350px,calc((100vw-1024px)/2-350px),-40px)] xl:mt-[clamp(-235px,calc((100vw-1280px)/2-235px),-40px)] 2xl:mt-0 3xl:mt-[clamp(-330px,calc((100vw-1920px)/2-330px),-40px)]
+    <section className="relative w-full bg-white  overflow-hidden lg:mt-[clamp(-372px,calc((100vw-1024px)/2-372px),-40px)] xl:mt-[clamp(-248px,calc((100vw-1280px)/2-248px),-40px)] 2xl:mt-0 3xl:mt-[clamp(-330px,calc((100vw-1920px)/2-330px),-40px)]
       ">
       <div className="absolute inset-0 z-0">
         <img
